a8ae403dd4320e6d59c7e9f52c2c772d0bd72916
mingyuan.pei@dianjoy.com <mingyuan.pei@dianjoy.com>
2016-10-25 19:33:35 +0800

    配置本地Mysql，中间件为PyMySQL，进行数据迁移

--- blog_django/settings.py
+++ blog_django/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'account',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -73,11 +75,15 @@ WSGI_APPLICATION = 'blog_django.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'mingyuan',
+        'PASSWORD': 'blog',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'CHARSET': 'utf8',
     }
 }
 
