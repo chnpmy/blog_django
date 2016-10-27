8f47af5d3950189cb3f6213c484737b448904aca
mingyuan.pei@dianjoy.com <mingyuan.pei@dianjoy.com>
2016-10-27 20:15:49 +0800

    配置static文件

--- blog_django/urls.py
+++ blog_django/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 import xadmin
+from django.conf.urls.static import static
+from django.conf import settings
 
 xadmin.autodiscover()
 from xadmin.plugins import xversion
@@ -27,3 +29,5 @@ urlpatterns = [
     url(r'^', include("blog.urls")),
 ]
 
+urlpatterns += static(settings.STATIC_URL,
+                      document_root=settings.STATIC_ROOT)
