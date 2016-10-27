708147e5a7393f4160b2a1ad30e6e0f310902182
mingyuan.pei@dianjoy.com <mingyuan.pei@dianjoy.com>
2016-10-27 19:22:23 +0800

    增加gunicorn和supervisor配置

---
+++ etc/gunicorn.py
@@ -0,0 +1,15 @@
+import multiprocessing
+
+bind = '127.0.0.1:8000'
+max_requests = 10000
+keepalive = 5
+
+proc_name = 'blog_diango'
+
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'gaiohttp'
+
+loglevel = 'info'
+errorlog = '-'
+
+x_forwarded_for_header = 'X-FORWARDED-FOR'
